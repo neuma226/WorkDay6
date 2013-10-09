38c8741b481db8b9077756d1008bae01368afd41
idsc3101 <idsc3101@gmail.com>
2013-10-09 15:14:37 -0500

    Work Day Wednesday

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,6 +3,20 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
+
+        Dim num As Integer
+
+        num = 1
+
+        Do While num <= 10
+            lstResult.Items.Add("Number is " & num ^ 2)
+            num = num + 1
+
+
+
+        Loop
+
+
         lstResult.Items.Add("Not implemented yet")
 
     End Sub
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,18 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim CurrentYear As Integer
+        Dim CurrentPopulation As Double
+
+        CurrentYear = 2013
+        CurrentPopulation = 7000000000.0
+
+        Do While (CurrentPopulation > 600000000.0)
+            CurrentPopulation = CurrentPopulation / 2
+            CurrentYear = CurrentYear - 50
+        Loop
+
+        MessageBox.Show("The year when the population was 6,000,000 people was " & CurrentYear, "Answer")
 
     End Sub
 End Class
